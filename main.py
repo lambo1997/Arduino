fde33a455da6435b00cf9bca3609a2add1f2c480
lambo1997 <115998563+lambo1997@users.noreply.github.com>
2022-10-20 20:59:56 +0900

    new txt
    output json from raw data

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ from datetime import datetime
 import time
 
 dt = datetime.now()
-print("start timestamp : ", dt)
 
-data = {}
-with open('data.txt') as f:
+
+data = []
+with open('0000146.txt') as f:
     lines = f.readlines()
 nn = len(lines)
+print(lines[1].split(";"))
 for i in range(1, nn):
-    if lines[i].split()[1] == "604":
+    if lines[i].split(";")[2] == "604":
         line =  lines[i]
-        values = line.split()[3:]
-        values = "".join(values)
+        values = line.split(";")[3]
         bit_pos = int(32 / 4)
         length = int(8 / 4)
         factor = 1
@@ -25,11 +25,12 @@ for i in range(1, nn):
         if res >= _min and res <= _max:
             dt_604 = datetime.now()
             print(dt_604, "BPI_Battery = ", res, unit)
-            data["BPI_Battery"] = res, unit
-    if lines[i].split()[1] == "100":
+            ddd = {}
+            ddd["BPI_Battery"] = res, unit
+            data.append(ddd)
+    if lines[i].split(";")[2] == "100":
         line =  lines[i]
-        values = line.split()[3:]
-        values = "".join(values)
+        values = line.split(";")[3]
         bit_pos = int(20 / 4)
         length = int(16 / 4)
         factor = 0.01
@@ -41,11 +42,12 @@ for i in range(1, nn):
         if res >= _min and res <= _max:
             dt_100 = datetime.now()
             print(dt_100, "Torque Sensor = ", res, unit)
-            data["Torque Sensor"] = res, unit
-    if lines[i].split()[1] == "101":
+            ddd = {}
+            ddd["Torque Sensor"] = res, unit
+            data.append(ddd)
+    if lines[i].split(";")[2] == "101":
         line =  lines[i]
-        values = line.split()[3:]
-        values = "".join(values)
+        values = line.split(";")[3]
         bit_pos = int(8 / 4)
         length = int(8 / 4)
         factor = 1
@@ -57,11 +59,12 @@ for i in range(1, nn):
         if res >= _min and res <= _max:
             dt_101 = datetime.now()
             print(dt_101, "Joystick = ", res, unit)
-            data["Joystick"] = res, unit
-    if lines[i].split()[1] == "614":
+            ddd = {}
+            ddd["Joystick"] = res, unit
+            data.append(ddd)
+    if lines[i].split(";")[2] == "614":
         line =  lines[i]
-        values = line.split()[3:]
-        values = "".join(values)
+        values = line.split(";")[3]
         bit_pos = int(32 / 4)
         length = int(8 / 4)
         factor = 1
@@ -73,11 +76,12 @@ for i in range(1, nn):
         if res >= _min and res <= _max:
             dt_614 = datetime.now()
             print(dt_614, "RPI_Battery = ", res, unit)
-            data["RPI_Battery"] = res, unit
-    if lines[i].split()[1] == "510":
+            ddd = {}
+            ddd["RPI_Battery"] = res, unit
+            data.append(ddd)
+    if lines[i].split(";")[2] == "510":
         line =  lines[i]
-        values = line.split()[3:]
-        values = "".join(values)
+        values = line.split(";")[3]
         bit_pos = int(48 / 4)
         length = int(16 / 4)
         factor = 0.01
@@ -89,10 +93,12 @@ for i in range(1, nn):
         if res >= _min and res <= _max:
             dt_510 = datetime.now()
             print(dt_510, "Motor Control = ", res, unit)
-            data["Motor Control"] = res, unit
+            ddd = {}
+            ddd["Motor Control"] = res, unit
+            data.append(ddd)
 
-json_object = json.dumps(data, indent = 5)
- 
+json_object = json.dumps(data, indent=5)
+print("data = ", data)
 # Writing to sample.json
-with open("result.json", "w") as outfile:
+with open("result_txt.json", "w") as outfile:
     outfile.write(json_object)
